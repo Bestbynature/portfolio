e3e3a5dbf2d04d27ae10d9ef90e109c6d43c1c8f
Dammy <bestbynature2010@gmail.com>
2023-03-06 16:11:52 +0100

    Modify the index.js file

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const modal = document.querySelector('.modal');
 const wrapper = document.querySelector('.wrapper1');
 const modalX = document.querySelector('.close');
 const links = document.querySelectorAll('.modal-link');
-
-
