2ba403ce767406c0183dee3b47814008d036c784
Dammy <bestbynature2010@gmail.com>
2023-03-06 14:01:37 +0100

    :hammer: Add the script file for the custom modal

---
+++ index.js
@@ -0,0 +1,35 @@
+const hamburger = document.querySelector('.burger');
+const modal = document.querySelector('.modal');
+const wrapper = document.querySelector('.wrapper1');
+const modalX = document.querySelector('.close');
+const links = document.querySelectorAll('.modal-link');
+
+function modalHide() {
+  modal.style.display = 'none';
+  wrapper.style.filter = 'none';
+}
+
+function windowClickHandler(event) {
+  if ((event.target === modal) || (event.target === links)) {
+    modalHide();
+  }
+}
+
+function modalShow() {
+  modal.style.display = 'flex';
+  wrapper.style.filter = 'blur(0.4rem)';
+  modal.scrollIntoView({ behavior: 'smooth' });
+}
+
+hamburger.addEventListener('click', () => {
+  modalShow();
+  window.addEventListener('click', windowClickHandler);
+});
+
+modalX.addEventListener('click', () => {
+  modalHide();
+  window.removeEventListener('click', windowClickHandler);
+});
+
+// script to handle each of the links on the modal
+
